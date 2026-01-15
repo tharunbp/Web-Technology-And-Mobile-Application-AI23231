32bc3a0ff77c289c19147eb636c66222da3d0adc
Tharun B P <tharun.bp.2024.aiml@rajalakshmi.edu.in>
2026-01-15 18:43:23 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,191 @@
+var iframe1=document.querySelector(".iframe1");
+
+var chennai=document.querySelector(".chennai");
+chennai.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Chennai";
+});
+
+var kanchipuram=document.querySelector(".kanchipuram");
+kanchipuram.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Kanchipuram";
+});
+
+var thiruvallur=document.querySelector(".thiruvallur");
+thiruvallur.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Tiruvallur_district";
+})
+
+var ariyalur=document.querySelector(".ariyalur");
+ariyalur.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Ariyalur_district";
+});
+
+var chengalpattu=document.querySelector(".chengalpattu");
+chengalpattu.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Chengalpattu_district";
+});
+
+var coimbatore=document.querySelector(".coimbatore");
+coimbatore.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Coimbatore_district";
+});
+
+var cuddalore=document.querySelector(".cuddalore");
+cuddalore.addEventListener("click",function(){  
+    iframe1.src="https://en.wikipedia.org/wiki/Cuddalore_district";
+});
+
+var dharmapuri=document.querySelector(".dharmapuri");
+dharmapuri.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Dharmapuri_district";
+});
+
+var dindigul=document.querySelector(".dindigul");
+dindigul.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Dindigul_district";
+});
+
+var erode=document.querySelector(".erode");
+erode.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Erode_district";
+});
+
+var kallakurichi=document.querySelector(".kallakurichi");
+kallakurichi.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Kallakurichi_district";
+});
+
+var kanyakumari=document.querySelector(".kanyakumari");
+kanyakumari.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Kanyakumari_district";
+});
+
+var karur=document.querySelector(".karur");
+karur.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Karur_district";
+});
+
+var krishnagiri=document.querySelector(".krishnagiri");
+krishnagiri.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Krishnagiri_district";
+});
+
+var madurai=document.querySelector(".madurai");
+madurai.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Madurai_district";
+});
+
+var mayiladuthurai=document.querySelector(".mayiladuthurai");
+mayiladuthurai.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Mayiladuthurai_district";
+});
+
+var nagapattinam=document.querySelector(".nagapattinam");
+nagapattinam.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Nagapattinam_district";
+});
+
+var namakkal=document.querySelector(".namakkal");
+namakkal.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Namakkal_district";
+});
+
+var nilgiris=document.querySelector(".nilgiris");
+nilgiris.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Nilgiris_district";
+});
+
+var perambalur=document.querySelector(".perambalur");
+perambalur.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Perambalur_district";
+});
+
+var pudukkottai=document.querySelector(".pudukkottai");
+pudukkottai.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Pudukkottai_district";
+});
+
+var ramanathapuram=document.querySelector(".ramanathapuram");
+ramanathapuram.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Ramanathapuram_district";
+});
+
+var ranipet=document.querySelector(".ranipet");
+ranipet.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Ranipet_district";
+});
+
+var salem=document.querySelector(".salem");
+salem.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Salem_district";
+});
+
+var sivagangai=document.querySelector(".sivagangai");
+sivagangai.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Sivaganga_district";
+});
+
+var tenkasi=document.querySelector(".tenkasi");
+tenkasi.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Tenkasi_district";
+});
+
+var thanjavur=document.querySelector(".thanjavur");
+thanjavur.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Thanjavur_district";
+});
+
+var theni=document.querySelector(".theni");
+theni.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Theni_district";
+});
+
+var thoothukudi=document.querySelector(".thoothukudi");
+thoothukudi.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Thoothukudi_district";
+});
+
+var tiruchirappalli=document.querySelector(".tiruchirappalli");
+tiruchirappalli.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Tiruchirappalli_district";
+});
+
+var tirunelveli=document.querySelector(".tirunelveli");
+tirunelveli.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Tirunelveli_district";
+});
+
+var tirupathur=document.querySelector(".tirupattur");
+tirupathur.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Tirupattur_district";
+});
+
+var tiruppur=document.querySelector(".tiruppur");
+tiruppur.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Tiruppur_district";
+});
+
+var tiruvannamalai=document.querySelector(".tiruvannamalai");
+tiruvannamalai.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Tiruvannamalai_district";
+});
+
+var tiruvarur=document.querySelector(".tiruvarur");
+tiruvarur.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Tiruvarur_district";
+});
+
+var vellore=document.querySelector(".vellore");
+vellore.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Vellore_district";
+});
+
+var viluppuram=document.querySelector(".viluppuram");
+viluppuram.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Viluppuram_district";
+});
+
+var virudhunagar=document.querySelector(".virudhunagar");
+virudhunagar.addEventListener("click",function(){
+    iframe1.src="https://en.wikipedia.org/wiki/Virudhunagar_district";
+});
